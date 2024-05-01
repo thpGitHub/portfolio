0616232c3e5b2e0d02d853cfa54d17b5bd02fd44
thp <thierrypoupon@hotmail.com>
2024-05-01 19:36:19 +0200

    working on contact page and animate background

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import "./style.css"; // Assurez-vous de créer ce fichier CSS
 import Image from "next/image";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 const ContactPage = () => {
   const handleSubmit = (event: React.FormEvent) => {
@@ -12,28 +13,28 @@ const ContactPage = () => {
 
   return (
     <div className="contact-container">
-      <div className="form-container">
-        <h1>Contactez-moi</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Nom:</label>
-          <input type="text" id="name" name="name" required />
+      <h1>Contactez-moi</h1>
+      <div className="content-container">
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
 
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
 
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" required></textarea>
-
-          <button type="submit">Envoyer</button>
-        </form>
-      </div>
-      <div className="image-container">
-        <Image
-          src="/DALL·E 2024-04-26 17.55.21 - An astronaut in full space suit sitting at a desk inside a space station, using a modern computer with lines of code visible on the screen. The astron.webp"
-          alt="Description de l'image"
-          layout="fill"
-          objectFit="cover"
-        />
+            <button type="submit">Envoyer</button>
+          </form>
+        </div>
+        <div className="image-container">
+          <Image
+            src="/DALL·E 2024-04-26 17.55.21 - An astronaut in full space suit sitting at a desk inside a space station, using a modern computer with lines of code visible on the screen. The astron.webp"
+            alt="Description de l'image"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <BackgroundBeams />
       </div>
     </div>
   );
