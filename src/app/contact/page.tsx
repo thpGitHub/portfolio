54ce201325bded8edcc26037346810d48fb22f94
thp <thierrypoupon@hotmail.com>
2024-05-05 21:40:43 +0200

    working on portfolio page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,27 +1,29 @@
 "use client";
-import React from "react";
-import "./style.css"; // Assurez-vous de créer ce fichier CSS
+import React, { useState } from "react";
+import "./style.css";
 import Image from "next/image";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { Highlight } from "@/components/ui/hero-highlight";
 
 const ContactPage = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    // Ici, vous pouvez ajouter le code pour gérer la soumission du formulaire
     console.log("Le formulaire a été soumis");
   };
 
   return (
     <div className="contact-container">
-      <Highlight className="text-black dark:text-white">
+      <Highlight className="dark:text-white">
         <h1>Contactez-moi</h1>
       </Highlight>
-      <div className="content-container">
-        <div className="form-container">
+      <div className="contact-content-container">
+        <div className="contact-form-container">
           <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
+            <input type="email" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
 
             <label htmlFor="message">Message:</label>
             <textarea id="message" name="message" required></textarea>
@@ -29,7 +31,7 @@ const ContactPage = () => {
             <button type="submit">Envoyer</button>
           </form>
         </div>
-        <div className="image-container">
+        <div className="contact-image-container">
           <Image
             src="/DALL·E 2024-04-26 17.55.21 - An astronaut in full space suit sitting at a desk inside a space station, using a modern computer with lines of code visible on the screen. The astron.webp"
             alt="Description de l'image"
