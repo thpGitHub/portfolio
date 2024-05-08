fcecb62b91c83823b3df488deff54ea619b65e90
thp <thierrypoupon@hotmail.com>
2024-05-08 21:25:21 +0200

    working on portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -6,41 +6,55 @@ import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "../../components//ui/tracing-beam";
 
 export default function TracingBeamDemo() {
+  const BackgroundImageUrl =
+    "/vaisseau-spatial-orbite-autour-comete-bleue-brillante-dans-galaxie-sombre-generee-par-ia.jpg";
   return (
-    <TracingBeam className="px-6">
-      <div className="max-w-2xl mx-auto antialiased pt-4 relative">
-        {dummyContent.map((item, index) => (
-          <div key={`content-${index}`} className="mb-10">
-            <div className="flex">
-              {item.badge.map((badge, badgeIndex) => (
-                <h2
-                  key={`badge-${badgeIndex}`}
-                  className={`${getBadgeColor(badge)} text-white rounded-full text-sm w-fit px-4 py-1 mb-4 mr-2`}
-                >
-                  {badge}
-                </h2>
-              ))}
-            </div>
+    <div
+      style={{
+        backgroundImage: `url(${BackgroundImageUrl})`,
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <TracingBeam className="px-6 bg-black bg-opacity-50">
+        <div className="max-w-2xl mx-auto antialiased pt-4 relative pb-[500px] text-white">
+          {dummyContent.map((item, index) => (
+            <div key={`content-${index}`} className="mb-10">
+              <div className="flex">
+                {item.badge.map((badge, badgeIndex) => (
+                  <h2
+                    key={`badge-${badgeIndex}`}
+                    className={`${getBadgeColor(
+                      badge
+                    )} text-white rounded-full text-sm w-fit px-4 py-1 mb-4 mr-2`}
+                  >
+                    {badge}
+                  </h2>
+                ))}
+              </div>
 
-            {/* <p className={twMerge(calsans.className, "text-xl mb-4")}> */}
-            <p className="text-xl mb-4">{item.title}</p>
+              {/* <p className={twMerge(calsans.className, "text-xl mb-4")}> */}
+              <p className="text-3xl mb-4">{item.title}</p>
 
-            <div className="text-sm  prose prose-sm dark:prose-invert">
-              {item?.image && (
-                <Image
-                  src={item.image}
-                  alt="blog thumbnail"
-                  height="1000"
-                  width="1000"
-                  className="rounded-lg mb-10 object-cover"
-                />
-              )}
-              {item.description}
+              <div className="text-sm  prose prose-sm dark:prose-invert">
+                {item?.image && (
+                  <Image
+                    src={item.image}
+                    alt="blog thumbnail"
+                    height="1000"
+                    width="1000"
+                    className="rounded-lg mb-10 object-cover"
+                  />
+                )}
+                {item.description}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </TracingBeam>
+          ))}
+        </div>
+      </TracingBeam>
+    </div>
   );
 }
 
@@ -120,23 +134,45 @@ const dummyContent = [
     image:
       "/DALL·E 2024-05-08 11.02.08 - An illustration of a Netflix clone.webp",
   },
+  {
+    title: "Application de paiement sans contact",
+    description: (
+      <>
+        <p>
+          Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
+          deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
+          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
+          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
+          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
+          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+        </p>
+      </>
+    ),
+    badge: ["Python", "Django"],
+    image:
+      "/DALL·E 2024-05-08 19.50.18 - An illustration of a contactless payment application.webp",
+  },
 ];
 
 function getBadgeColor(badge: string) {
   switch (badge) {
-    case 'Next.js':
-      return 'bg-red-500';
-    case 'Node.js':
-      return 'bg-green-500';
-    case 'React':
-      return 'bg-blue-500';
-    case 'Laravel':
-      return 'bg-yellow-500';
-    case 'Vue.js':
-      return 'bg-green-500';
-    case 'Express.js':
-      return 'bg-gray-500';
+    case "Next.js":
+      return "bg-red-500";
+    case "Node.js":
+      return "bg-green-500";
+    case "React":
+      return "bg-blue-500";
+    case "Laravel":
+      return "bg-yellow-500";
+    case "Vue.js":
+      return "bg-green-500";
+    case "Express.js":
+      return "bg-gray-500";
+    case "Python":
+      return "bg-blue-500";
+    case "Django":
+      return "bg-green-500";
     default:
-      return 'bg-black';
+      return "bg-black";
   }
 }
